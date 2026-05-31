b9d9a00b2d8ad95f7956d44c27acf089247ad27d
Cinnamon-jp <cinnamon312.dev@gmail.com>
2026-05-31 16:23:59 +0900

    [AI]重回帰モデルを作成

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import src.simple_linear_regression
+import src.multiple_linear_regression
 
 def main():
-    src.simple_linear_regression.main()
-
+    # src.simple_linear_regression.main()
+    src.multiple_linear_regression.main()
 
 if __name__ == "__main__":
     main()
