e35d8f2f67290bce7640faf35e561942bc516dce
Cinnamon-jp <cinnamon312.dev@gmail.com>
2026-06-24 08:35:27 +0900

    モデルの定義を追加

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import src.simple_linear_regression_jax
 import src.simple_linear_regression
 import src.multiple_linear_regression
+import src.number_classification
 
 
 def main() -> None:
-    src.simple_linear_regression_jax.main()
+    # src.simple_linear_regression_jax.main()
     # src.simple_linear_regression.main()
     # src.multiple_linear_regression.main()
+    src.number_classification.main()
 
 
 if __name__ == "__main__":
--- src/number_classification.py
+++ src/number_classification.py
@@ -11,7 +11,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 class NumberClassificationModel(nnx.Module):
-    def __init__:
-        
+    # 入出力層・隠れ層を定義
+    def __init__(self, in_features: int, hidden_features: int, out_features: int, rngs: nnx.Rngs):
+        self.layer1 = nnx.Linear(in_features, hidden_features, rngs=rngs)
+        self.layer2 = nnx.Linear(hidden_features, hidden_features, rngs=rngs)
+        self.layer3 = nnx.Linear(hidden_features, hidden_features, rngs=rngs)
+        self.out_layer = nnx.Linear(hidden_features, out_features, rngs=rngs)
 
-def main():
+    # 順伝播を定義
+    def __call__(self, x: jax.Array) -> jax.Array:
+        x = jax.nn.relu(self.layer1(x))
+        x = jax.nn.relu(self.layer2(x))
+        x = jax.nn.relu(self.layer3(x))
+        return self.out_layer(x)
+
+def main() -> None:
+    pass
